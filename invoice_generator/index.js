84ac45c0487406c34250e7bab23f8917ca9238a7
atharva <88647398+atharvahanjankar@users.noreply.github.com>
2022-01-09 09:07:38 +0530

    Add files via upload

---
+++ invoice_generator/index.js
@@ -0,0 +1,20 @@
+function res(){
+var a=document.getElementById('var1').value;
+console.log(a);
+var b=document.getElementById('var2').value;
+var summ= a * b ;
+document.getElementById('sum').innerHTML=summ;
+}
+
+function res2(){
+    var c=document.getElementById('var3').value;
+    var d=document.getElementById('var4').value;
+    var summ1= c * d ;
+    document.getElementById('sum1').innerHTML=summ1;
+    }
+    function res3(){
+        var e=document.getElementById('var5').value;
+        var f=document.getElementById('var6').value;
+        var summ3= e * f ;
+        document.getElementById('sum2').innerHTML=summ3;
+        }
